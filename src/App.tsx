a8ecdff70d4254d9bf5db39f0a9e71149aa3a909
pebblepark <subling02@gmail.com>
2022-02-10 22:51:26 +0900

    Feat: Axios를 사용하는 간단한 예제 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import React from 'react';
 
 function App() {
-    return <div>App</div>;
+    const [isLoading, setLoading] = useState(false);
+    const [isError, setError] = useState(false);
+    const [data, setData] = useState<{ url?: string }>({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setError(false);
+            setLoading(true);
+
+            try {
+                const response = await axios('https://random.dog/woof.json');
+
+                setData(response.data);
+            } catch (error) {
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (isError) return <h1>Error, try again...!</h1>;
+    if (isLoading) return <h1>Loading...</h1>;
+    return (
+        <div>
+            <img src={data.url} />
+        </div>
+    );
 }
 
 export default App;
